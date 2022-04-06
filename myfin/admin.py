24a2ae5177cd9f8d0bea15019a0ff3775f1b3b25
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-04-06 16:33:08 +0300

    Update admin.py

--- myfin/admin.py
+++ myfin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Wallets, Ballance, Categories, Transactions
+
+admin.site.register(Wallets)
+admin.site.register(Ballance)
+admin.site.register(Categories)
+admin.site.register(Transactions)
