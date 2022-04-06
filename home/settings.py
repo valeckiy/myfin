8fe9951e2868d0e40f29caf92f7c909a0f8def05
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-04-06 15:04:13 +0300

    Update settings.py

--- home/settings.py
+++ home/settings.py
@@ -16,8 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -117,9 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-#STATIC_URL = '/static'
-STATIC_URL = '/static/'
-
+#STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'myfin/static')
 
 
 # Default primary key field type
