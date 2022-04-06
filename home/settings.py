b73cb139442f3b9246db81fe863b31e1323efc6b
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-04-06 15:46:19 +0300

    Update settings.py

--- home/settings.py
+++ home/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+STATIC_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
