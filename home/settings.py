a32a3b10fe62762eafca2e6a3f80b8133bb3cc2f
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-04-06 15:38:32 +0300

    Update settings.py

--- home/settings.py
+++ home/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/home/ubuntu/home/myfin/static/',
+    '/home/ubuntu/home/myfin/myfin/static/',
 ]
 
 #STATICFILES_DIRS = [
