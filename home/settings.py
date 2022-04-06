7706c4c622a8243d5d854f5129574d9055c216f3
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-04-06 15:34:23 +0300

    Update settings.py

--- home/settings.py
+++ home/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 #    os.path.join(BASE_DIR, 'myfin/static'),
 #]
 #TEMPLATES_DIRS = (os.path.join(BASE_DIR, 'templates'))
-STATIC_URL = os.path.join(BASE_DIR, 'myfin/static/')
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
