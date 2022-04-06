a545daa1378b529321e42327db511270f3428bb5
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-04-06 15:15:04 +0300

    Update settings.py

--- home/settings.py
+++ home/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 #STATIC_URL = '/static/'
 #STATIC_URL = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/home/myfin/myfin/static/'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
