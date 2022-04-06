782d867540a25ef2c130d49cc45f9fbb722e6c66
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-04-06 14:59:39 +0300

    Update settings.py

--- home/settings.py
+++ home/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 #STATIC_URL = '/static'
 STATIC_URL = 'home/ubuntu/home/myfin/static/'
+STATIC_ROOT = 'home/ubuntu/home/myfin/static/'
 
 
 # Default primary key field type
