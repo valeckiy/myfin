73927a3a89ac197fa4459810bb31386ebfb37a6a
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-04-08 10:23:33 +0300

    1

--- myfin/admin.py
+++ myfin/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from .models import Wallets, Ballance, Categories, Transactions
 
-admin.site.register(Wallets)
+# admin.site.register(Wallets)
 admin.site.register(Ballance)
 admin.site.register(Categories)
 admin.site.register(Transactions)
+
+class WalletsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+# Register the admin class with the associated model
+admin.site.register(Wallets, WalletsAdmin)
