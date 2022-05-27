fa1f19905f92952e9fcbbe8ec2be41fc4f39c17a
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-05-27 15:20:20 +0300

    1
    1

--- home/wsgi.py
+++ home/wsgi.py
@@ -1,13 +1,5 @@
-"""
-WSGI config for home project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.settings')
-
-application = get_wsgi_application()
