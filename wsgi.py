9c4433ddad678d3e591d5ffa5927fbd9c0c60e68
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-05-27 14:37:27 +0300

    d
    d

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 import os
+import sys
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, 'project_name'))
+sys.path.insert(0, root_path)
+
 
 from django.core.wsgi import get_wsgi_application
 
