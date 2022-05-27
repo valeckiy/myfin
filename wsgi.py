e266b369f1950475bc25c7d7419496b7add12b85
valeckiy <102744213+valeckiy@users.noreply.github.com>
2022-05-27 14:43:05 +0300

    1
    1

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,8 @@
 import os
 import sys
-root_path = os.path.abspath(os.path.split(__file__)[0])
-sys.path.insert(0, os.path.join(root_path, 'project_name'))
-sys.path.insert(0, root_path)
-
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.settings')
 
 application = get_wsgi_application()
